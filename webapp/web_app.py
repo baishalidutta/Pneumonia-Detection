6ca55c24b6c1f74066c1f297c3ff921fcbc36031
Baishali Dutta <me@itsbaishali.com>
2022-04-26 22:20:29 +0200

    Minor grammatical corrections

--- webapp/web_app.py
+++ webapp/web_app.py
@@ -33,8 +33,8 @@ description = "This application uses a Convolutional Neural Network (CNN) model 
               "a chosen X-ray shows if the person has pneumonia disease or not. To check the model " \
               "prediction, here are the true labels of the provided examples below: the first 4 images " \
               "belong to normal whereas the last 4 images are of pneumonia category. More specifically, " \
-              "the 5th and 6th images are viral pneumonia infection in nature whereas he last 2 images " \
-              "are bacterial infection in nature."
+              "the 5th and 6th images are of type viral pneumonia infection in nature whereas he last 2 images " \
+              "are of bacterial infection in nature."
 
 gr.Interface(fn=make_prediction,
              inputs=image_input,
